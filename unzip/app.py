7993b494b616a25378fc7d548d6922ce339ba7f9
Steven Cook <awscook@amazon.com>
2023-05-18 08:35:58 +1000

    Formatting

--- unzip/app.py
+++ unzip/app.py
@@ -4,28 +4,31 @@ import os
 import shutil
 import uuid
 
-ZIP_FILE_NAME = 'audio.zip'
-OUTPUT_BUCKET_NAME = os.environ['OUTPUT_BUCKET_NAME']
+ZIP_FILE_NAME = "audio.zip"
+OUTPUT_BUCKET_NAME = os.environ["OUTPUT_BUCKET_NAME"]
 
-s3 = boto3.client('s3')
-transcribe = boto3.client('transcribe')
+s3 = boto3.client("s3")
+transcribe = boto3.client("transcribe")
 
-# Note this code only handles the case where one s3 record is recieved 
-# the code should handle an array of records and loop over the zip files. 
-# Also note, that the maximum number of files in zip that could be processed this way 
-# is 10000 due to the limit of 10000 queued transcribe jobs. 
+
+# Note this code only handles the case where one s3 record is recieved
+# the code should handle an array of records and loop over the zip files.
+# Also note, that the maximum number of files in zip that could be processed this way
+# is 10000 due to the limit of 10000 queued transcribe jobs.
 def lambda_handler(event, context):
     print(f"Received event: {event}")
-    s3.download_file(event['Records'][0]['s3']['bucket']['name'], 
-                     event['Records'][0]['s3']['object']['key'], 
-                     f"/tmp/{ZIP_FILE_NAME}")
+    s3.download_file(
+        event["Records"][0]["s3"]["bucket"]["name"],
+        event["Records"][0]["s3"]["object"]["key"],
+        f"/tmp/{ZIP_FILE_NAME}",
+    )
 
     # Unzip
-    with zipfile.ZipFile(f"/tmp/{ZIP_FILE_NAME}", 'r') as zip_ref:
+    with zipfile.ZipFile(f"/tmp/{ZIP_FILE_NAME}", "r") as zip_ref:
         zip_ref.extractall("/tmp/audio")
 
     job_run = uuid.uuid4()
-    
+
     # list all files in the /tmp/audio directory
     for file in os.listdir("/tmp/audio"):
         print(f"Uploading {file} to S3")
@@ -35,31 +38,23 @@ def lambda_handler(event, context):
             CallAnalyticsJobName=f"Redact-{job_run}-{file}",
             Media={
                 "MediaFileUri": f"s3://{OUTPUT_BUCKET_NAME}/input/{file}",
-                "RedactedMediaFileUri": f"s3://{OUTPUT_BUCKET_NAME}/input/{file}"
+                "RedactedMediaFileUri": f"s3://{OUTPUT_BUCKET_NAME}/input/{file}",
             },
             OutputLocation=f"s3://{OUTPUT_BUCKET_NAME}/output/",
             Settings={
-                'ContentRedaction': {
-                    'RedactionType': 'PII',
-                    'RedactionOutput': 'redacted',
-                    'PiiEntityTypes': [
-                        'ALL',
-                    ]
-            },
-            'LanguageOptions': [
-                'en-US'
-            ]
+                "ContentRedaction": {
+                    "RedactionType": "PII",
+                    "RedactionOutput": "redacted",
+                    "PiiEntityTypes": [
+                        "ALL",
+                    ],
+                },
+                "LanguageOptions": ["en-US"],
             },
             ChannelDefinitions=[
-            {
-                'ChannelId': 0,
-                'ParticipantRole': 'AGENT'
-            },
-            {
-                'ChannelId': 1,
-                'ParticipantRole': 'CUSTOMER'
-            }
-            ]
+                {"ChannelId": 0, "ParticipantRole": "AGENT"},
+                {"ChannelId": 1, "ParticipantRole": "CUSTOMER"},
+            ],
         )
         print(res)
 
